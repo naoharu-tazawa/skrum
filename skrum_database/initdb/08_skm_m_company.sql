2037df86f723f5e8855c78cff15bc25b007b2000
nohr-local <nohr.est@gmail.com>
2017-04-16 19:53:33 +0900

    テスト用初期データの追加

---
+++ skrum_database/initdb/08_skm_m_company.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: 127.0.0.1    Database: skm
+-- ------------------------------------------------------
+-- Server version	5.7.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `m_company`
+--
+
+LOCK TABLES `m_company` WRITE;
+/*!40000 ALTER TABLE `m_company` DISABLE KEYS */;
+INSERT INTO `m_company` VALUES (1,'会社名','ヴィジョンヴィジョンヴィジョンヴィジョンヴィジョン','ミッションミッションミッションミッションミッション',NULL,'1','company1','2017-04-08 15:59:53','2017-04-08 17:04:34',NULL),(2,'会社名','ヴィジョンヴィジョンヴィジョンヴィジョンヴィジョン','ミッションミッションミッションミッションミッション',NULL,'1','company2','2017-04-08 16:00:44','2017-04-08 17:05:17',NULL),(3,'会社名','ヴィジョンヴィジョンヴィジョンヴィジョンヴィジョン','ミッションミッションミッションミッションミッション',NULL,'1','company3','2017-04-08 16:01:28','2017-04-08 17:05:36',NULL),(4,'会社名','ヴィジョンヴィジョンヴィジョンヴィジョンヴィジョン','ミッションミッションミッションミッションミッション',NULL,'1','company4','2017-04-08 16:02:09','2017-04-08 17:05:55',NULL),(5,'会社名','ヴィジョンヴィジョンヴィジョンヴィジョンヴィジョン','ミッションミッションミッションミッションミッション',NULL,'1','company5','2017-04-08 16:02:39','2017-04-08 17:06:25',NULL);
+/*!40000 ALTER TABLE `m_company` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-08 18:56:33
