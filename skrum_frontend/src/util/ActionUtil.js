6472ad9b36057870fa3899da63a4e286b79b84a2
nohr-local <nohr.est@gmail.com>
2017-05-11 06:32:09 +0900

    サブドメインの追加

--- skrum_frontend/src/util/ActionUtil.js
+++ skrum_frontend/src/util/ActionUtil.js
@@ -1,12 +1,13 @@
 export const keyValueIdentity = (key, value) => ({ [key]: value });
 
-const defaultToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhY2NvdW50cy5za3J1bS5qcCIsImlhdCI6MTQ5MTYzNDc5MSwiZXhwIjoxNTA3MTg2NzkxLCJzZG0iOiJjb21wYW55MSIsInVpZCI6MSwiY2lkIjoxLCJyaWQiOiJBMDAzIiwicmx2Ijo3LCJwZXJtaXNzaW9ucyI6WyJjb21wYW55X3Byb2ZpbGVfZWRpdCIsInVzZXJfYWRkIiwidXNlcl9kZWxldGUiLCJ1c2VyX3Blcm1pc3Npb25fY2hhbmdlIiwicGFzc3dvcmRfcmVzZXQiLCJ0aW1lZnJhbWVfZWRpdCIsInBsYW5fZWRpdCIsInBheW1lbnRfdmlldyJdfQ.5qhe2t9cCJLC3bN-aqVOI52Ur4hme7nAB1i8dcVi5Og';
-export const extractToken = () => {
-  // const { token } = status.auth;
-  return defaultToken;
+export const extractToken = (status) => {
+  const { token } = status.auth;
+  return token;
 };
 
-export const extractDomain = (status) => {
-  const { company = 'company1' } = status.auth;
-  return company;
+export const extractDomain = () => {
+  const path = location.hostname;
+  const bases = path.split('.');
+  const subdomain = bases[0];
+  return subdomain;
 };
