14637940012bbcd8b7aeb8318bbac166f946f85c
nohr-local <nohr.est@gmail.com>
2017-06-06 20:49:24 +0900

    AM0601会社基本情報取得 - 新規追加

--- skrum_backend/src/AppBundle/Controller/Api/CompanyController.php
+++ skrum_backend/src/AppBundle/Controller/Api/CompanyController.php
@@ -8,6 +8,7 @@ use AppBundle\Controller\BaseController;
 use AppBundle\Exception\ApplicationException;
 use AppBundle\Exception\JsonSchemaException;
 use AppBundle\Utils\Permission;
+use AppBundle\Api\ResponseDTO\NestedObject\BasicCompanyInfoDTO;
 
 /**
  * 会社コントローラ
@@ -16,6 +17,32 @@ use AppBundle\Utils\Permission;
  */
 class CompanyController extends BaseController
 {
+    /**
+     * 会社基本情報取得
+     *
+     * @Rest\Get("/v1/companies/{companyId}.{_format}")
+     * @Permission(value="company_profile_edit")
+     * @param Request $request リクエストオブジェクト
+     * @param string $companyId 会社ID
+     * @return BasicCompanyInfoDTO
+     */
+    public function getCompanyAction(Request $request, string $companyId): BasicCompanyInfoDTO
+    {
+        // 認証情報を取得
+        $auth = $request->get('auth_token');
+
+        // 会社IDの一致をチェック
+        if ($companyId != $auth->getCompanyId()) {
+            throw new ApplicationException('会社IDが存在しません');
+        }
+
+        // 会社基本情報取得
+        $companyService = $this->getCompanyService();
+        $basicCompanyInfoDTO = $companyService->getBasicCompanyInfo($companyId);
+
+        return $basicCompanyInfoDTO;
+    }
+
     /**
      * 会社基本情報変更
      *
