0cc09020104128f7e4a19b1617a5f99a61fca6a5
Adrian Khong <kckhong@gmail.com>
2017-06-09 10:45:34 +0900

    Enabled responsive size of LineChart in OKRObjectiveProgressChart.

--- skrum_frontend/src/project/OKRDetails/OKRObjectiveProgressChart.js
+++ skrum_frontend/src/project/OKRDetails/OKRObjectiveProgressChart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Line } from 'recharts';
+import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Line } from 'recharts';
 import _ from 'lodash';
 import { ProgressSeriesPropTypes } from './propTypes';
 import { convertToRelativeTimeText, DateFormat } from '../../util/DatetimeUtil';
@@ -17,41 +17,43 @@ export default class OKRObjectiveProgressChart extends Component {
       ({ datetime, progress: _.toNumber(achievementRate) }));
     const percentFormatter = percent => (percent ? `${percent}%` : '');
     const dateFormatter = date => `${convertToRelativeTimeText(date, { format: DateFormat.YMD })}`;
-    const renderTooltip = ([{ payload } = {}]) => (!payload ? null : (
+    const renderTooltip = ([{ payload } = {}]) => (payload && (
       <div className={styles.tooltip} key={`tooltip-item-${payload.datetime}`}>
         <li>{dateFormatter(payload.datetime)}</li>
         <li>達成値: {payload.progress}%</li>
       </div>));
     return (
       <div className={styles.component}>
-        <LineChart
-          className={styles.progressChart}
-          width={300} height={280} data={progressData}
-          margin={{ top: 40, right: 40, bottom: 20, left: 0 }}
-        >
-          <CartesianGrid vertical={false} stroke="#ECF0F3" />
-          <XAxis
-            dataKey="datetime"
-            axisLine={false}
-            stroke="#626A7E"
-            tickFormatter={dateFormatter}
-          />
-          <YAxis
-            domain={['auto', 'auto']}
-            axisLine={false}
-            tickLine={false}
-            stroke="#626A7E"
-            tickFormatter={percentFormatter}
-          />
-          <Tooltip content={({ payload }) => renderTooltip(payload)} />
-          <Line
-            isAnimationActive={false}
-            dataKey="progress"
-            stroke="#626A7E"
-            strokeWidth={8}
-            dot={false}
-          />
-        </LineChart>
+        <ResponsiveContainer width="100%" aspect={300 / 280}>
+          <LineChart
+            className={styles.progressChart}
+            data={progressData}
+            margin={{ top: 40, right: 40, bottom: 20, left: 0 }}
+          >
+            <CartesianGrid vertical={false} stroke="#ECF0F3" />
+            <XAxis
+              dataKey="datetime"
+              axisLine={false}
+              stroke="#626A7E"
+              tickFormatter={dateFormatter}
+            />
+            <YAxis
+              domain={['auto', 'auto']}
+              axisLine={false}
+              tickLine={false}
+              stroke="#626A7E"
+              tickFormatter={percentFormatter}
+            />
+            <Tooltip content={({ payload }) => renderTooltip(payload)} />
+            <Line
+              isAnimationActive={false}
+              dataKey="progress"
+              stroke="#626A7E"
+              strokeWidth={8}
+              dot={false}
+            />
+          </LineChart>
+        </ResponsiveContainer>
       </div>);
   }
 }
