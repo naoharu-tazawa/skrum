46180bcd7d80cb1cd1352b9532cb175602426724
nohr-local <nohr.est@gmail.com>
2017-06-14 20:40:00 +0900

    Timeline - Implemented post submit.

--- skrum_frontend/src/project/Timeline/TimelineContainer.js
+++ skrum_frontend/src/project/Timeline/TimelineContainer.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { reset } from 'redux-form';
 import PostListContainer from './PostList/PostListContainer';
 import { explodePath, isPathFinal } from '../../util/RouteUtil';
+import { errorType } from '../../util/PropUtil';
 import styles from './TimelineContainer.css';
 import { fetchGroupPosts, postGroupPosts } from './action';
+import Form from './Form';
 
 class TimelineContainer extends Component {
 
   static propTypes = {
     isFetching: PropTypes.bool,
+    isPosting: PropTypes.bool,
     pathname: PropTypes.string,
     dispatchFetchGroupPosts: PropTypes.func,
     dispatchPostGroupPosts: PropTypes.func,
+    dispatchResetForm: PropTypes.func,
+    error: errorType,
   };
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   componentWillMount() {
     const { pathname } = this.props;
@@ -31,42 +42,42 @@ class TimelineContainer extends Component {
       const { id } = explodePath(pathname);
       dispatchFetchGroupPosts(id);
     }
+    this.result(next);
   }
 
-  handleSubmit(e) {
-    const { pathname } = this.props;
-    const { id } = explodePath(pathname);
-    const target = e.target;
-    e.preventDefault();
-    this.props.dispatchPostGroupPosts(
-      id,
-      target.post.value.trim(),
-      '1',
-    );
+  result(props = this.props) {
+    const { isPosting, error, dispatchResetForm } = props;
+    if (!isPosting && !error) {
+      dispatchResetForm();
+    }
+  }
+
+  handleSubmit(data) {
+    if (data.post !== undefined) {
+      const { pathname } = this.props;
+      const { id } = explodePath(pathname);
+      this.props.dispatchPostGroupPosts(
+        id,
+        data.post,
+        '1',
+      );
+    }
   }
 
   render() {
     if (this.props.isFetching) {
       return <div className={styles.spinner} />;
     }
-    const { pathname } = this.props;
+    const { isPosting, error, pathname } = this.props;
     return (
       <div className={styles.container}>
         <div className={`${styles.inner} ${styles.timeline}`}>
           <section className={styles.new_post_box}>
-            <form onSubmit={e => this.handleSubmit(e)}>
-              <h1 className={styles.ttl_section}>新規投稿作成</h1>
-              <div className={styles.cont_box}>
-                <div className={styles.user_name}>
-                  <dl>
-                    <dt><img src="/img/common/icn_user.png" alt="" /></dt>
-                    <dd />
-                  </dl>
-                </div>
-                <div className={styles.text_area}><textarea id="post" placeholder="仕事の状況はどうですか？" /></div>
-                <div className={styles.btn}><button>投稿する</button></div>
-              </div>
-            </form>
+            <Form
+              isPosting={isPosting}
+              error={error}
+              onSubmit={this.handleSubmit}
+            />
           </section>
           <PostListContainer pathname={pathname} />
         </div>
@@ -75,17 +86,18 @@ class TimelineContainer extends Component {
 }
 
 const mapStateToProps = (state) => {
-  const { isFetching = false } = state.timeline || {};
+  const { isFetching = false, isPosting = false, error } = state.timeline || {};
   const { locationBeforeTransitions } = state.routing || {};
   const { pathname } = locationBeforeTransitions || {};
-  return { isFetching, pathname };
+  return { isFetching, isPosting, error, pathname };
 };
 
 const mapDispatchToProps = (dispatch) => {
   const dispatchFetchGroupPosts = groupId => dispatch(fetchGroupPosts(groupId));
   const dispatchPostGroupPosts = (groupId, post, disclosureType) =>
     dispatch(postGroupPosts(groupId, post, disclosureType));
-  return { dispatchFetchGroupPosts, dispatchPostGroupPosts };
+  const dispatchResetForm = () => dispatch(reset('form'));
+  return { dispatchFetchGroupPosts, dispatchPostGroupPosts, dispatchResetForm };
 };
 
 export default connect(
